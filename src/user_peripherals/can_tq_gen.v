ed54d4098b8bfc6f68fe76ccb4c89a3c20719852
Rahul Bhagwat <rahulmbme@gmail.com>
2025-09-04 16:23:45 +0530

    some cleanup

--- src/user_peripherals/can_tq_gen.v
+++ src/user_peripherals/can_tq_gen.v
@@ -19,34 +19,49 @@ module can_tq_gen #(
     output logic [4:0] tq_position    // Which TQ within current bit (1 to total_tq)
 );
 
+  logic [5:0] brp_cnt;
+  logic [4:0] tq_cnt;
+
+  typedef enum {
+    SYNC,
+    PROP,
+    PHASE1,
+    PHASE2
+  } fsm_e;
+  fsm_e segment_position, next_segment_position;
 
   always @(posedge clk) begin
     if (!rst_n) begin
-      tq_tick      <= 1'b0;
-      bit_tick     <= 1'b0;
-      sample_point <= 1'b0;
-      sync_seg     <= 1'b0;
-      prop_seg     <= 1'b0;
-      phase_seg1   <= 1'b0;
-      phase_seg2   <= 1'b0;
-      tq_position  <= 5'd1;
-    end else begin
-
-    end
-  end
-
-  // Time Quanta Gen
-  logic [5:0] brp_cnt;
-  always_ff @(posedge clk) begin
-    if (!rst_n) begin
-      brp_cnt <= 0;
-      tq_tick <= 0;
+      tq_tick               <= 1'b0;
+      bit_tick              <= 1'b0;
+      sample_point          <= 1'b0;
+      sync_seg              <= 1'b0;
+      prop_seg              <= 1'b0;
+      phase_seg1            <= 1'b0;
+      phase_seg2            <= 1'b0;
+      brp_cnt               <= 1'b0;
+      tq_position           <= 5'd1;
+      segment_position      <= SYNC;
+      next_segment_position <= SYNC;
     end else if (brp_cnt == brp) begin
       brp_cnt <= 0;
       tq_tick <= 1;
     end else begin
       brp_cnt <= brp_cnt + 1;
       tq_tick <= 0;
+    end
+  end
+
+  // Segment Counter
+  always_comb begin
+    next_segment_position = segment_position;  // continue with current as default
+    if (tq_tick) begin
+      unique case (segment_position)
+        SYNC:   ;
+        PROP:   ;
+        PHASE1: ;
+        PHASE2: ;
+      endcase
     end
   end
 
